45871fbce09764ba9895bf3ee254b13b4d44e87d
Artem Gerus <artem.gerus@TLL1-15745.local>
2020-02-14 12:58:58 +0200

    add custom

--- demo/index.js
+++ demo/index.js
@@ -1,10 +1,27 @@
 import { init, createPublicTokenProvider } from '../src/index';
 import config from './config';
 
+const cfg = config;
+try {
+  // You can use custom.js for customising config object
+  // Example:
+  //
+  // export default {
+  //   apiUrl: 'http://localhost:3001',
+  //   apiKey: '1231223',
+  // };
+
+  // eslint-disable-next-line global-require
+  const custom = require('./custom.js').default;
+  Object.assign(cfg, custom);
+} catch (e) {
+  console.log(`Error: ${e}`);
+}
+
 try {
   const customerId = 24;
-  const tokenProvider = createPublicTokenProvider(config.apiUrl, config.apiKey, customerId);
-  init(config, tokenProvider);
+  const tokenProvider = createPublicTokenProvider(cfg.apiUrl, cfg.apiKey, customerId);
+  init(cfg, tokenProvider);
 } catch (e) {
   console.log(`Error: ${e.message}`);
 }

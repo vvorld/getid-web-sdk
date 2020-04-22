2385eec9f95cf7539b97f17fac92c7a27e4aa69d
Anna Teterina <anna@idcredit.info>
2020-04-22 14:57:38 +0300

    add babel polyfill to webpack

--- demo/index.js
+++ demo/index.js
@@ -1,11 +1,6 @@
-import 'react-app-polyfill/ie9';
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
-
 import { init, createPublicTokenProvider } from '../src/index';
 import config from './config';
 
-const cfg = { ...config };
 try {
   // You can use custom.js for customising config object
   // Example:
@@ -17,15 +12,15 @@ try {
 
   // eslint-disable-next-line global-require
   const custom = require('./custom.js').default;
-  Object.assign(cfg, custom);
+  Object.assign(config, custom);
 } catch (e) {
   console.log(`Error: ${e}`);
 }
 
 try {
   const customerId = Math.floor(Math.random() * 1000000);
-  const tokenProvider = createPublicTokenProvider(cfg.apiUrl, cfg.apiKey, customerId);
-  init(cfg, tokenProvider);
+  const tokenProvider = createPublicTokenProvider(config.apiUrl, config.apiKey, customerId);
+  init(config, tokenProvider);
 } catch (e) {
   console.log(`Error: ${e.message}`);
 }
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 require('@babel/polyfill');
-
+const path = require('path');
 const merge = require('webpack-merge');
 
 const common = require('./webpack.common.js');
@@ -9,5 +9,19 @@ module.exports = merge(common, {
   mode: 'development',
   entry: {
     demo: ['@babel/polyfill', './demo/index.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        include: path.resolve(__dirname, 'demo'),
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+    ],
   },
 });

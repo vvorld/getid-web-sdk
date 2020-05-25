81e62de4acc5ec51f18ab7bace5a2c4ed4a84dc8
Anna Teterina <anna@idcredit.info>
2020-05-25 14:56:23 +0300

    add word wrap

--- src/components/inputs/file-input/style.js
+++ src/components/inputs/file-input/style.js
@@ -3,7 +3,7 @@ import { fade, makeStyles } from '@material-ui/core';
 const style = makeStyles((theme) => ({
   outlinedInput: {
     border: 'none',
-    lineHeight: 'normal',
+    lineHeight: '12px',
     padding: '14px 9px',
     color: theme.palette.blueDark,
     marginRight: '7px',
@@ -35,6 +35,7 @@ const style = makeStyles((theme) => ({
     },
   },
   labelContainer: {
+    // width: '70%',
     paddingLeft: '5px',
     display: 'flex',
     flexDirection: 'column-reverse',
@@ -44,7 +45,7 @@ const style = makeStyles((theme) => ({
     color: `${fade(theme.palette.deepBlue, 0.5)}`,
     fontSize: '16px',
     textAlign: 'left',
-    lineHeight: '22px',
+    lineHeight: '16px',
     display: 'none',
     zIndex: -1,
     '&.selected': {
@@ -57,10 +58,12 @@ const style = makeStyles((theme) => ({
     },
   },
   label: {
+    wordWrap: 'break-word',
+    wordBreak: 'break-all',
     textAlign: 'left',
     color: `${fade(theme.palette.deepBlue, 0.5)}`,
     fontSize: '16px',
-    lineHeight: '22px',
+    lineHeight: '16px',
     paddingLeft: '5px',
     '&.selected': {
       paddingLeft: '5px',

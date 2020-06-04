25dc8e99f2deac574fbe6b5ee32f4c04294ea713
Rostyslav <rostyslav.piulyk@devabit.com>
2020-06-04 11:32:17 +0300

    revert last commit; remove webrtc-adapter as it slow edge performance

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
-import 'webrtc-adapter';
 import './polyfills/toBlob.polyfill';
 import './polyfills/webrtc.polyfill.min';
 import { renderMainComponent } from './main-module';

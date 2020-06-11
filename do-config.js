f138853832040bc68dac28db082b6bcd563d64cf
Anya <nlyaii@users.noreply.github.com>
2020-06-11 13:16:32 +0300

    Delete do-config.js

--- do-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  bucketAccessKeyId: '2MSWOPV5ARQC6WWRDNZM',
-  bucketSecret: 'PnKuaNNGTbkJN8DMlXgfKXXwiMbV7bakpLZ0MbjPQ7o',
-  bucketName: 'getid-storage',
-  bucketUrl: 'https://fra1.digitaloceanspaces.com',
-};

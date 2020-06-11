773cf6d3f4c1d9a5b05adcaa30f2e4794f8cb52b
OlegPavlyuk22 <oleg.pavlyuk@devabit.com>
2020-06-11 09:27:49 +0300

    try to integrate to ac

--- src/main-module.js
+++ src/main-module.js
@@ -1,4 +1,7 @@
+import { create } from 'jss';
+import camelCase from 'jss-plugin-camel-case';
 import { ThemeProvider } from '@material-ui/styles';
+import { StylesProvider, jssPreset } from '@material-ui/core/styles';
 import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -8,20 +11,39 @@ import TranslationsContext from './context/TranslationsContext';
 import Main from './layouts/Main';
 import ErrorBoundary from './layouts/ErrorBoundary';
 
+// const generateClassName = createGenerateClassName({
+//   productionPrefix: 'get-id',
+// });
+
+const styleNode = document.createComment('jss-insertion-point');
+document.head.insertBefore(styleNode, document.head.firstChild);
+
+const jss = create({
+  ...jssPreset(),
+  insertionPoint: 'jss-insertion-point',
+});
+
+console.log('JSS:');
+console.dir(jss);
+
+jss.use(camelCase());
+
 const MainModule = (widgetOptions) => (
-  <ThemeProvider theme={MainTheme}>
-    <Provider store={store}>
-      <TranslationsContext.Provider
-        value={{ translations: widgetOptions.translations }}
-      >
-        <ErrorBoundary>
-          <Main
-            {...widgetOptions}
-          />
-        </ErrorBoundary>
-      </TranslationsContext.Provider>
-    </Provider>
-  </ThemeProvider>
+  <StylesProvider jss={jss}>
+    <ThemeProvider theme={MainTheme}>
+      <Provider store={store}>
+        <TranslationsContext.Provider
+          value={{ translations: widgetOptions.translations }}
+        >
+          <ErrorBoundary>
+            <Main
+              {...widgetOptions}
+            />
+          </ErrorBoundary>
+        </TranslationsContext.Provider>
+      </Provider>
+    </ThemeProvider>
+  </StylesProvider>
 );
 
 /**
@@ -29,5 +51,10 @@ const MainModule = (widgetOptions) => (
  * @param widgetOptions
  */
 export const renderMainComponent = (widgetOptions) => {
-  ReactDOM.render(MainModule(widgetOptions), document.getElementById(widgetOptions.containerId));
+  const container = document.getElementById(widgetOptions.containerId);
+  const component = MainModule(widgetOptions);
+
+  if (container.hasChildNodes()) ReactDOM.unmountComponentAtNode(container);
+
+  ReactDOM.render(component, container);
 };

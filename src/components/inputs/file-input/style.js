0feab0e59b10d77d23fc2251596fb298d80126be
Anna Teterina <anna@idcredit.info>
2020-06-19 15:02:10 +0300

    bug fix

--- src/components/inputs/file-input/style.js
+++ src/components/inputs/file-input/style.js
@@ -20,6 +20,7 @@ const style = makeStyles((theme) => ({
     flexDirection: 'row-reverse',
     backgroundColor: 'transparent',
     width: '100%',
+    marginTop: '5px',
     height: 51,
     '&.selected': {
       borderColor: theme.palette.violet.main,

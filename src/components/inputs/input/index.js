0813c03f2cfdea4e09aa8cc51168db716560022e
Anna Teterina <anna@idcredit.info>
2020-07-30 15:03:34 +0300

    WIP: error styles

--- src/components/inputs/input/index.js
+++ src/components/inputs/input/index.js
@@ -1,10 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Input = ({
   name, label, value, onChange, required,
 }) => {
   const placeholder = label + (required ? '*' : '');
   const [currValue, setValue] = useState(value);
+  const [error, setError] = useState(null);
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  const validate = (checkValue) => {
+    if (currValue && currValue.length !== 0 && !re.test(String(checkValue).toLowerCase())) {
+      setError('Input is not valid');
+      return;
+    }
+    setError(null);
+  };
+
+  useEffect(() => {
+    validate(currValue);
+  }, [currValue]);
+
   return (
     <div>
       <input
@@ -12,6 +27,7 @@ const Input = ({
         name={name}
         placeholder={placeholder}
         required={required}
+        className={error && 'getid-error'}
         value={currValue}
         onChange={(e) => {
           const newValue = e.target.value;
@@ -20,6 +36,7 @@ const Input = ({
         }}
         key={`input-${label}`}
       />
+      {error && <span className="getid-error__message">{error }</span>}
     </div>
   );
 };

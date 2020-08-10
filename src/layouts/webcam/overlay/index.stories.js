9a6c0f60c3112714f72aac43fd8124f3f0964819
Artem Gerus <artem.gerus@TLL1-15745.local>
2020-08-10 09:12:49 +0300

    add ellips stories

--- src/layouts/webcam/overlay/index.stories.js
+++ src/layouts/webcam/overlay/index.stories.js
@@ -4,6 +4,8 @@ import createOverlay from './index';
 export default { title: 'Webcam|Overlay' };
 
 const Overlay = createOverlay('rectangle', 3 / 2);
+const Ellips = createOverlay('ellips', 2 / 3);
+
 export const Rectangle_480x240 = () => (
   <div style={{
     width: '480px', height: '240px', background: 'red', position: 'relative',
@@ -37,5 +39,32 @@ export const Rectangle_320x420 = () => (
   }}
   >
     <Overlay width={320} height={420} />
+  </div>
+);
+
+export const Ellips_320x240 = () => (
+  <div style={{
+    width: '320px', height: '240px', background: 'red', position: 'relative',
+  }}
+  >
+    <Ellips width={320} height={240} />
+  </div>
+);
+
+export const Ellips_240x320 = () => (
+  <div style={{
+    width: '240px', height: '320px', background: 'red', position: 'relative',
+  }}
+  >
+    <Ellips width={240} height={320} />
+  </div>
+);
+
+export const Ellips_1x2 = () => (
+  <div style={{
+    width: '240px', height: '480px', background: 'red', position: 'relative',
+  }}
+  >
+    <Ellips width={240} height={480} />
   </div>
 );

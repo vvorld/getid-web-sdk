82790eeeb9e9b99b666483deae098bbee6ad2468
Gerus Artem <artgeruz@gmail.com>
2020-08-25 07:43:29 +0300

    fix photo size

--- src/layouts/webcam/camera/helpers.js
+++ src/layouts/webcam/camera/helpers.js
@@ -1,18 +1,24 @@
 const frameRenderer = (webcam, {
   left, right, top, bottom,
 }) => (callback) => {
-  const canvas = document.createElement('canvas');
-  canvas.width = right - left;
-  canvas.height = bottom - top;
-  const context = canvas.getContext('2d');
-
   const additionalVOffset = top * 0.1;
   const additionalHOffset = left * 0.1;
+
+  const t = top - additionalVOffset;
+  const l = left - additionalHOffset;
+  const r = right + additionalHOffset;
+  const b = bottom + additionalVOffset;
+
+  const canvas = document.createElement('canvas');
+  canvas.width = r - l;
+  canvas.height = b - t;
+  const context = canvas.getContext('2d');
+
   context.drawImage(webcam,
-    left - additionalHOffset,
-    top - additionalVOffset,
-    right - left + 2 * additionalHOffset,
-    bottom - top + 2 * additionalVOffset,
+    l,
+    t,
+    r - l,
+    b - t,
     0, 0, canvas.width, canvas.height);
   canvas.toBlob(callback, 'image/jpeg', 1.0);
 };

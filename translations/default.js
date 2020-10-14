5739af488a88b9375695bc014b5e703f67905844
Artem Gerus <artgeruz@gmail.com>
2020-10-14 14:37:19 +0300

    Create default.js

---
+++ translations/default.js
@@ -0,0 +1,299 @@
+export default {
+  CountryAndDocument_header: 'Choose document type',
+  CountryAndDocument_subHeader: 'Carefully choose document type of document you want to provide for identification',
+  CountryAndDocument_next: 'Go next',
+  CountryAndDocument_back: 'Go back',
+  CountryAndDocument_countryPlaceholder: 'Choose country',
+
+  PhotoRules_header: 'Take a front side picture of ID Document',
+  PhotoRules_subHeader: ' ',
+  PhotoRules_line_1: 'Make sure ID Document is in focus',
+  PhotoRules_line_2: 'Avoid glare and counter light',
+  PhotoRules_line_3: 'Make sure all corners of the ID Document are visible',
+  PhotoRules_line_4: 'Do not cover any important data',
+  PhotoRules_back: 'Go back',
+  PhotoRules_next: 'ok, i got it',
+  /**/
+  IdCapture_guide_header: 'Take a front side picture of ID Document',
+  IdCapture_guide_subHeader: ' ',
+  IdCapture_guide_next: 'ok, i got it',
+  IdCapture_guide_back: 'Go back',
+
+  /**/
+  IdCapture_record_header: 'Take a front side picture of ID Document',
+  IdCapture_record_subHeader: ' ',
+  IdCapture_record_next: 'take a picture',
+  IdCapture_record_back: 'Go back',
+
+  /**/
+  IdCapture_preview_header: 'Captured ID document',
+  IdCapture_preview_subHeader: 'Is document fully visible and data clearly readable?',
+  IdCapture_preview_next: 'Yes, continue',
+  IdCapture_preview_back: 'No, retake',
+
+  IdCapture_checking_header: 'Captured ID document',
+  IdCapture_checking_subHeader: ' ',
+  IdCapture_checking_back: 'Cancel',
+
+  IdCapture_retake_description_header: 'Captured ID document',
+  IdCapture_retake_description_subHeader: ' ',
+  IdCapture_retake_description_next: 'Retake',
+  IdCapture_retake_description_back: 'Continue',
+
+  IdCaptureBack_guide_header: 'Take a back side picture of ID Document',
+  IdCaptureBack_guide_subHeader: ' ',
+  IdCaptureBack_guide_next: 'ok, i got it',
+  IdCaptureBack_guide_back: 'Go back',
+
+  IdCaptureBack_record_header: 'Take a back side picture of ID Document',
+  IdCaptureBack_record_subHeader: ' ',
+  IdCaptureBack_record_next: 'take a picture',
+  IdCaptureBack_record_back: 'Go back',
+
+  IdCaptureBack_checking_header: 'Captured ID document',
+  IdCaptureBack_checking_subHeader: ' ',
+  IdCaptureBack_checking_back: 'Cancel',
+
+  IdCaptureBack_preview_header: 'Captured ID document',
+  IdCaptureBack_preview_subHeader: 'Is document fully visible and data clearly readable?',
+  IdCaptureBack_preview_next: 'Yes, continue',
+  IdCaptureBack_preview_back: 'No, retake',
+
+  IdCaptureBack_retake_description_header: 'Captured ID document',
+  IdCaptureBack_retake_description_subHeader: ' ',
+  IdCaptureBack_retake_description_next: 'Retake',
+  IdCaptureBack_retake_description_back: 'Continue',
+
+  Form_header: 'Profile data',
+  Form_subHeader: 'Please provide personal details same as on your ID Document',
+  Form_next: 'Go next',
+  Form_back: 'Go back',
+  Form_FileTooltip: 'Allowed formats: jpg, jpeg, png, pdf.',
+
+  Const_day: 'Day',
+  Const_year: 'Year',
+  Const_month: 'Month',
+  Const_january: 'January',
+  Const_february: 'February',
+  Const_march: 'March',
+  Const_april: 'April',
+  Const_may: 'May',
+  Const_june: 'June',
+  Const_july: 'July',
+  Const_august: 'August',
+  Const_september: 'September',
+  Const_october: 'October',
+  Const_november: 'November',
+  Const_december: 'December',
+
+  Const_passport: 'Passport',
+  'Const_id-card': 'ID Card',
+  'Const_residence-permit': 'Residence Permit',
+  'Const_driving-licence': 'Drivers License',
+
+  IdSelfie_record_header: 'Take a selfie',
+  IdSelfie_record_subHeader: ' ',
+  IdSelfie_record_next: 'Take a photo',
+  IdSelfie_record_back: 'Go back',
+
+  IdSelfie_guide_header: 'Take a selfie',
+  IdSelfie_guide_subHeader: ' ',
+  IdSelfie_guide_next: 'ok, i got it',
+  IdSelfie_guide_back: 'Go back',
+
+  IdSelfie_preview_header: 'Captured selfie',
+  IdSelfie_preview_subHeader: 'Is your face clearly visible?',
+  IdSelfie_preview_next: 'Yes, continue',
+  IdSelfie_preview_back: 'No, retake',
+
+  IdSelfie_retake_description_header: 'Captured selfie',
+  IdSelfie_retake_description_subHeader: ' ',
+  IdSelfie_retake_description_next: 'Retake',
+  IdSelfie_retake_description_back: 'Continue',
+
+  SelfieRules_header: 'Take a selfie',
+  SelfieRules_subHeader: ' ',
+  SelfieRules_line_1: 'Make sure lightning conditions are sufficient',
+  SelfieRules_line_2: 'Avoid flash and counter light',
+  SelfieRules_line_3: 'Avoid other people in the frame',
+  SelfieRules_line_4: 'Look straight ahead',
+  SelfieRules_back: 'Go back',
+  SelfieRules_next: 'ok, i got it',
+
+  Recording_guide_header: 'Record video',
+  Recording_guide_subHeader: 'You will be asked to record a short video saying a phrase provided',
+  Recording_guide_next: 'ok, i got it',
+  Recording_guide_back: 'Go back',
+
+  Recording_record_header: 'Are you ready?',
+  Recording_record_subHeader: ' ',
+  Recording_record_next: 'Start record',
+  Recording_record_back: 'Go back',
+
+  Recording_recording_header: 'Please, follow the instructions',
+  Recording_recording_subHeader: ' ',
+  Recording_recording_nextPhrase: 'Next phrase',
+  Recording_recording_lastPhrase: 'Finish',
+  Recording_recording_prevPhrase: 'Previous phrase',
+  Recording_recording_back: 'Go back',
+  Recording_recording_phrasesHeader: 'Please say on camera:',
+
+  Recording_preview_header: 'Check the recorded video',
+  Recording_preview_subHeader: ' ',
+  Recording_preview_next: 'It\'s ok',
+  Recording_preview_back: 're-take a video',
+
+  ThankYou_header: 'Thank you',
+  ThankYou_subHeader: 'The identity verification process is complete',
+  ThankYou_next: 'Continue',
+  ThankYou_back: '',
+
+  LivenessReady_header: 'Liveness check',
+  LivenessReady_subHeader: 'You will be asked to perform some actions. The face should be always inside the frame.',
+  LivenessReady_next: 'I am ready, let’s go',
+  LivenessReady_back: 'Go back',
+
+  LivenessCommand_header: 'Liveness check',
+  LivenessCommand_subHeader: 'You will be asked to perform some actions. The face should be always inside the frame.',
+  LivenessCommand_next: 'Go next',
+  LivenessCommand_again: 'Try again',
+  LivenessCommand_undestand: 'OK, i undestand',
+  LivenessCommand_back: 'Go back',
+
+  LivenessError_multipleFaces: 'Multiple faces',
+  LivenessError_noFace: 'No face, please try again',
+  LivenessError_timeout: 'Timeout, please try again',
+  LivenessError_slowConnection: 'Slow connection',
+  LivenessError_tooManyFrames: 'Too many frames',
+  LivenessError_faceDetectionError: 'Face is not detected',
+  LivenessError_sessionError: 'Session error, please try again',
+  LivenessError_tooManyWarnings: 'Too many warnings, please try again',
+
+  LivenessCommand_smile: 'Smile',
+  LivenessCommand_closeEyes: 'Close your eyes',
+  LivenessCommand_turnRight: 'Turn your head to the right',
+  LivenessCommand_turnLeft: 'Turn your head to the left',
+  LivenessCommand_tiltLeft: 'Tilt your head to the left',
+  LivenessCommand_tiltRight: 'Tilt your head to the right',
+  LivenessCommand_thanks: 'Good! Thanks',
+  LivenessCommand_expected: 'Expected',
+  LivenessCommand_detected: 'Detected',
+  LivenessCommand_success: 'Great!',
+
+  ChangeDevice_header: 'Continue on your phone',
+  ChangeDevice_subHeader: 'Here\'s how to do it:',
+  ChangeDevice_next: 'get secure link',
+  ChangeDevice_back: 'back',
+  ChangeDevice_line_1: 'Send a secure link to your phone',
+  ChangeDevice_line_2: 'Open the link and complete the tasks',
+  ChangeDevice_line_3: 'Check back here to finish the submission',
+
+  MagicLink_header: 'Get your secure link',
+  MagicLink_subHeader: 'Open your smartphone camera with Photo mode and hover it on a QR code you see bellow',
+  MagicLink_back: 'Back',
+
+  // ERROR
+  retake_description_error: 'Document can not be identified',
+  retake_description_unknown: 'Document can not be identified',
+  retake_description_selfie_unknown: 'Selfie can not be identified',
+  retake_description_selfie_error: 'Selfie can not be identified',
+  retake_description_front_side_missing: 'Front side wasn\'t recognized.',
+  retake_description_back_side_missing: 'Back side wasn\'t recognized.',
+
+  // container ID  is not provided
+  container_missmatch_error: 'Please provide container id.',
+
+  // token is not provided
+  token_missmatch_error: 'Token provider must be a function that returns promise or jwt response object',
+
+  // schema_missmatch
+  schema_missmatch_error: 'Insecure protocol detected',
+  schema_missmatch_error_header: 'Insecure protocol detected',
+  schema_missmatch_error_subHeader: 'Please use https protocol',
+
+  // Broswer not supported
+  browser_not_supported_error: 'Browser is not supported',
+  browser_not_supported_error_header: 'Browser is not supported',
+  browser_not_supported_error_variants: 'Use another browser:',
+  browser_not_supported_error_subHeader: 'Please use newer version of your browser or contact your provider for further information.',
+
+  // Device doesn't have camera
+  no_camera_error: 'Device has no camera',
+  no_camera_error_header: 'Your device has no camera',
+  no_camera_error_subHeader: 'Please use device with camera',
+
+  camera_not_allowed_error: 'Failed to access your camera',
+  camera_not_allowed_error_header: 'Camera is not supported',
+  camera_not_allowed_error_subHeader: 'Web camera access in your browser settings.',
+
+  camera_generic_error: 'Failed to access your camera',
+  camera_generic_error_header: 'Camera is not supported',
+  camera_generic_error_subHeader: 'To make a photo you need to give access for webcam',
+
+  sdkkey_invalid_error_header: 'Wrong SDK key',
+  sdkkey_invalid_error_error_header: 'Wrong SDK key',
+  sdkkey_invalid_error_subHeader: 'Please contact your provider for further information.',
+
+  // Server return 400
+  '400_error': 'Service is unavailable',
+  '400_error_header': 'Bad request',
+  '400_error_subHeader': 'Try again or contact the support team.',
+
+  // Server return 500
+  '500_error': 'Service is unavailable',
+  '500_error_header': 'Service is unavailable',
+  '500_error_subHeader': 'Try again or contact the support team.',
+
+  // App Exist application
+  customerid_exists_error: 'Application already exists.',
+  customerid_exists_error_header: 'Application already exists.',
+  customerid_exists_error_subHeader: 'Please contact your provider for further information.',
+
+  // Api version missmatch
+  api_version_missmatch_error: 'Sdk version not compatible with current api version',
+  api_version_missmatch_error_header: 'Your sdk version not compatible with current api version',
+  api_version_missmatch_error_subHeader: 'Please update your sdk version',
+
+  // Server is not unavailable (liveness or recording fallback)
+  server_unavailable_error: 'Server is unavailable',
+  server_unavailable_error_header: 'Server is unavailable',
+  server_unavailable_error_subHeader: 'Please contact your provider for further information.',
+
+  // Application didn't send
+  isFail_error: 'Cannot create application',
+  isFail_error_header: 'Cannot send data',
+  isFail_error_subHeader: 'Error occurs when you try to send data to the server.',
+
+  // We don't know what to do
+  internal_error: 'Internal error',
+  internal_error_header: 'Something was wrong',
+  internal_error_subHeader: 'Please contact your provider for further information.',
+
+  token_malformed_error: 'Incorrect link',
+  token_malformed_error_header: 'Something was wrong',
+  token_malformed_error_subHeader: 'Please contact your provider for further information.',
+
+  token_invalid_error: 'Incorrect link',
+  token_invalid_error_header: 'Incorrect link',
+  token_invalid_error_subHeader: 'Please contact your provider for further information.',
+
+  token_empty_error: 'Token link',
+  token_empty_error_header: 'Incorrect link',
+  token_empty_error_subHeader: 'Please contact your provider for further information.',
+
+  token_expired_error: 'Link is no longer valid',
+  token_expired_error_header: 'Your link is no longer valid',
+  token_expired_error_subHeader: 'Please contact your provider for further information.',
+
+  // ERROR BUTTONS
+  error_next: 'Retry',
+  error_back: 'Cancel',
+  error_finish: 'Ok',
+
+  error_changedevice_button: 'Change device and continue',
+
+  // OTHER
+  mobileCamera_landscape: 'For a more convenient user experience, we ask you to turn the phone into portrait mode',
+  openPopUp_button: 'open modal',
+  closePopUp_button: 'Close',
+};
